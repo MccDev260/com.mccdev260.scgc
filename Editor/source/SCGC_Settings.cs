b5ffd9fc4d355d742f4c1aed95c90a1714a66c66
Jon McCreadie <jjmcc2660@gmail.com>
2023-04-18 18:39:51 +0100

    Squashed commit of the following:
    commit 45c0500d118ace68be7d887d1f57f8d5afc9479f
    Author: Jon McCreadie <jjmcc2660@gmail.com>
    Date:   Tue Apr 18 18:19:09 2023 +0100
    
        Update mccdev260.scgpc.Editor.asmdef
    
    commit 4cdf5b8f0caf709a023b34d87c96825c668b7005
    Author: Jon McCreadie <jjmcc2660@gmail.com>
    Date:   Tue Apr 18 18:15:39 2023 +0100
    
        Update SCGC_Settings.cs
    
        Add create asset menu attribute.
    
    commit e9b5ea708c0ffce7874d9d01e44d1d71b5d93c8d
    Author: Jon McCreadie <jjmcc2660@gmail.com>
    Date:   Tue Apr 18 18:14:42 2023 +0100
    
        Delete settings asset.
    
        Can't edit values from packages folder.

--- Editor/source/SCGC_Settings.cs
+++ Editor/source/SCGC_Settings.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "SCGC_Settings", menuName = "ScriptableObjects/EditorTools/SCGC_Setings", order = 0)]
 public class SCGC_Settings : ScriptableObject
 {   
     public Vector3 focusOffset = new Vector3(0f, 0f, -5f);
