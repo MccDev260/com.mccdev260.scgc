abd29e9d0d6e077d9f0eaa44e0a897ca66ac5118
Jon McCreadie <jjmcc2660@gmail.com>
2023-04-18 20:17:06 +0100

    Encapsulate in own namespace.

--- Editor/SceneCamGamepadControl.cs
+++ Editor/SceneCamGamepadControl.cs
@@ -4,85 +4,89 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEditor;
 
-[InitializeOnLoad()]
-public class SceneCamGamepadControl : Editor
+namespace MccDev260.Tools
 {
-    public static SCGC_Settings settings;
-
-    static SceneCamGamepadControl()
+    [InitializeOnLoad()]
+    public class SceneCamGamepadControl : Editor
     {
-        EditorApplication.update += Update;
-    }
+        public static SCGC_Settings settings;
 
-    public static Vector3 pos;
-    public static Quaternion rot;
-
-    static void Update()
-    {
-        if (settings != null)
+        static SceneCamGamepadControl()
         {
-            var gamepad = Gamepad.current;
-            var keyboard = Keyboard.current;
+            EditorApplication.update += Update;
+        }
 
-            if (gamepad == null) return;
+        public static Vector3 pos;
+        public static Quaternion rot;
 
-            SceneView view = SceneView.lastActiveSceneView;
-            pos = view.pivot;
-            rot = view.rotation;
-
-            // Controls.
-            Vector2 inputMove = gamepad.leftStick.ReadValue();
-            Vector2 inputRotate = gamepad.rightStick.ReadValue();
-            bool inputUp = gamepad.rightShoulder.isPressed;
-            bool inputDown = gamepad.leftShoulder.isPressed;
-            bool inputSelect = gamepad.aButton.isPressed;
-            bool inputfocus = gamepad.bButton.isPressed;
-
-            GameObject camTarget = new GameObject("SceneViewCamTarget");
-            camTarget.transform.position = pos;
-            camTarget.transform.rotation = rot;
-
-            // Obj Selection.
-            if (inputSelect)
+        static void Update()
+        {
+            if (settings != null)
             {
-                if (Physics.Raycast(camTarget.transform.position, camTarget.transform.forward, out RaycastHit hit, settings.raycastDistance))
+                var gamepad = Gamepad.current;
+                var keyboard = Keyboard.current;
+
+                if (gamepad == null) return;
+
+                SceneView view = SceneView.lastActiveSceneView;
+                pos = view.pivot;
+                rot = view.rotation;
+
+                // Controls.
+                Vector2 inputMove = gamepad.leftStick.ReadValue();
+                Vector2 inputRotate = gamepad.rightStick.ReadValue();
+                bool inputUp = gamepad.rightShoulder.isPressed;
+                bool inputDown = gamepad.leftShoulder.isPressed;
+                bool inputSelect = gamepad.aButton.isPressed;
+                bool inputfocus = gamepad.bButton.isPressed;
+
+                GameObject camTarget = new GameObject("SceneViewCamTarget");
+                camTarget.transform.position = pos;
+                camTarget.transform.rotation = rot;
+
+                // Obj Selection.
+                if (inputSelect)
                 {
-                    if (hit.transform.gameObject)
+                    if (Physics.Raycast(camTarget.transform.position, camTarget.transform.forward, out RaycastHit hit, settings.raycastDistance))
                     {
-                        Selection.activeGameObject = hit.transform.gameObject;
+                        if (hit.transform.gameObject)
+                        {
+                            Selection.activeGameObject = hit.transform.gameObject;
+                        }
                     }
                 }
-            }
 
-            // Focus.
-            if (Selection.activeGameObject != null)
-            {
-                if (keyboard.fKey.isPressed || inputfocus)
+                // Focus.
+                if (Selection.activeGameObject != null)
                 {
-                    var activeObjPos = Selection.activeGameObject.transform.position;
-                    camTarget.transform.position = activeObjPos + settings.focusOffset;
-                    camTarget.transform.LookAt(activeObjPos);
+                    if (keyboard.fKey.isPressed || inputfocus)
+                    {
+                        var activeObjPos = Selection.activeGameObject.transform.position;
+                        camTarget.transform.position = activeObjPos + settings.focusOffset;
+                        camTarget.transform.LookAt(activeObjPos);
+                    }
                 }
+
+                camTarget.transform.position += camTarget.transform.forward * inputMove.y * settings.lateralSpeed * Time.deltaTime;
+                camTarget.transform.position += camTarget.transform.right * inputMove.x * settings.lateralSpeed * Time.deltaTime;
+                camTarget.transform.position += inputUp ? camTarget.transform.up * settings.upSpeed * Time.deltaTime : camTarget.transform.up * 0f;
+                camTarget.transform.position -= inputDown ? camTarget.transform.up * settings.downSpeed * Time.deltaTime : camTarget.transform.up * 0f;
+
+                camTarget.transform.Rotate(Vector3.up, inputRotate.x * settings.rotationSpeed * Time.deltaTime);
+                camTarget.transform.Rotate(Vector3.right, -inputRotate.y * settings.rotationSpeed * Time.deltaTime);
+                camTarget.transform.rotation = Quaternion.Euler(camTarget.transform.eulerAngles.x,
+                    camTarget.transform.eulerAngles.y, 0f);
+
+                pos = camTarget.transform.position;
+                rot = camTarget.transform.rotation;
+
+                view.pivot = pos;
+                view.rotation = rot;
+                view.size = 0f;
+
+                GameObject.DestroyImmediate(camTarget);
             }
-
-            camTarget.transform.position += camTarget.transform.forward * inputMove.y * settings.lateralSpeed * Time.deltaTime;
-            camTarget.transform.position += camTarget.transform.right * inputMove.x * settings.lateralSpeed * Time.deltaTime;
-            camTarget.transform.position += inputUp ? camTarget.transform.up * settings.upSpeed * Time.deltaTime : camTarget.transform.up * 0f;
-            camTarget.transform.position -= inputDown ? camTarget.transform.up * settings.downSpeed * Time.deltaTime : camTarget.transform.up * 0f;
-
-            camTarget.transform.Rotate(Vector3.up, inputRotate.x * settings.rotationSpeed * Time.deltaTime);
-            camTarget.transform.Rotate(Vector3.right, -inputRotate.y * settings.rotationSpeed * Time.deltaTime);
-            camTarget.transform.rotation = Quaternion.Euler(camTarget.transform.eulerAngles.x,
-                camTarget.transform.eulerAngles.y, 0f);
-
-            pos = camTarget.transform.position;
-            rot = camTarget.transform.rotation;
-
-            view.pivot = pos;
-            view.rotation = rot;
-            view.size = 0f;
-
-            GameObject.DestroyImmediate(camTarget);
         }
     }
 }
+
